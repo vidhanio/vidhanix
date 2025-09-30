46cd5e307e49eacabd004e241f5e03cf8308b547
Vidhan Bhatt <me@vidhan.io>
2025-09-30 16:11:31 -0400

    chore: rebuild system

--- users/vidhanio/default.nix
+++ users/vidhanio/default.nix
@@ -1,6 +1,7 @@
 {
-  lib,
   pkgs,
+  osConfig,
+  lib,
   ...
 }:
 {
@@ -16,11 +17,6 @@
     };
   };
 
-  age.secrets.wakatime = {
-    file = ../../secrets/wakatime.age;
-    path = ".wakatime.cfg";
-  };
-
   impermanence = {
     directories = [
       "Downloads"
@@ -29,6 +25,44 @@
       ".cache/nix"
     ];
   };
+
+  programs = {
+    _1password.enable = true;
+    bat.enable = true;
+    cider-2.enable = true;
+    direnv.enable = true;
+    eza.enable = true;
+    firefox.enable = true;
+    fish.enable = true;
+    gh = {
+      enable = true;
+      username = "vidhanio";
+    };
+    ghostty.enable = true;
+    git = {
+      enable = true;
+      userEmail = "me@vidhan.io";
+    };
+    neovim.enable = true;
+    ripgrep.enable = true;
+    vesktop.enable = true;
+    vscode.enable = true;
+    zoxide.enable = true;
+  };
+
+  services = {
+    wakatime.enable = true;
+  };
+
+  home.packages = with pkgs; [
+    berkeley-mono-variable
+    pragmata-pro-variable
+    jetbrains-mono
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  xdg.autostart.enable = lib.mkIf osConfig.nixpkgs.hostPlatform.isLinux true;
 
   home.stateVersion = "25.05";
 }
