054415911e64fa323580121351611bb716eac51d
Vidhan Bhatt <me@vidhan.io>
2025-09-30 23:26:04 -0400

    chore: rebuild system

--- modules/home/programs/nixcord.nix
+++ modules/home/programs/nixcord.nix
@@ -9,9 +9,14 @@ in
 lib.mkIf cfg.enable {
   programs.nixcord = {
     vesktop.enable = true;
+    config.themeLinks = [
+      "https://raw.githubusercontent.com/ashtrath/Tokyo-Night/main/themes/tokyo-night.theme.css"
+    ];
   };
 
   xdg.autostart.entries = map lib.getDesktop [ cfg.finalPackage.vesktop ];
 
   impermanence.directories = [ ".config/vesktop/sessionData" ];
+
+  stylix.targets.nixcord.enable = false;
 }
