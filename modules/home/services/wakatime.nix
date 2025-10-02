c29edbe6ea502bcf1fd660943b413cabcba6da18
Vidhan Bhatt <me@vidhan.io>
2025-10-01 23:27:40 -0400

    chore: rebuild system

--- modules/home/services/wakatime.nix
+++ modules/home/services/wakatime.nix
@@ -27,7 +27,7 @@ in
         wakatime-cli = lib.getExe pkgs.wakatime;
       in
       lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        export XDG_RUNTIME_DIR="~/Downloads"
+        export XDG_RUNTIME_DIR=''${XDG_RUNTIME_DIR:-/run/user/$(id -u)}
         run ${wakatime-cli} --config-write api_key="$(cat ${config.age.secrets.wakatime.path})"
       '';
   };
