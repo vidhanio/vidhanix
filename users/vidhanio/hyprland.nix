82c055c0502d683477bf06350fc4629bc61e0bc1
Vidhan Bhatt <me@vidhan.io>
2025-10-05 19:37:20 -0400

    chore: rebuild system

--- users/vidhanio/hyprland.nix
+++ users/vidhanio/hyprland.nix
@@ -33,12 +33,17 @@ lib.mkIf (osConfig ? programs.hyprland) {
         "$mainMod, mouse:272, movewindow"
         "$mainMod, mouse:273, resizewindow"
       ];
-      bindle = [
-        ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
-      ];
 
-      input.accel_profile = "flat";
+      bindel = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ];
+      bindl = [
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86AudioNext, exec, playerctl next"
+      ];
 
       decoration = {
         rounding = 10;
