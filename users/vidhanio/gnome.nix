341d95b938624275f8fa2e091224c5ab970f7f95
Vidhan Bhatt <me@vidhan.io>
2025-10-05 21:44:57 -0400

    chore: rebuild system

---
+++ users/vidhanio/gnome.nix
@@ -0,0 +1,65 @@
+{
+  lib,
+  osConfig,
+  pkgs,
+  ...
+}:
+let
+  osCfg = osConfig.services.desktopManager.gnome;
+  osDconfCfg = osConfig.programs.dconf;
+in
+{
+  programs.gnome-shell = {
+    enable = lib.mkIf (osConfig ? services.desktopManager.gnome) osCfg.enable;
+    extensions =
+      with pkgs.gnomeExtensions;
+      map (package: { inherit package; }) [
+        appindicator
+        blur-my-shell
+        dash-to-panel
+        pip-on-top
+        rounded-window-corners-reborn
+      ];
+  };
+
+  dconf.settings = lib.mkIf (osConfig ? programs.dconf && osDconfCfg.enable) {
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-applications = [ ];
+      switch-applications-backward = [ ];
+      switch-windows = [ "<Alt>Tab" ];
+      switch-windows-backward = [ "<Shift><Alt>Tab" ];
+    };
+
+    "org/gnome/shell" = {
+      favorite-apps = [
+        "org.gnome.Nautilus.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "firefox.desktop"
+        "code-insiders.desktop"
+        "vesktop.desktop"
+        "cider-2.desktop"
+      ];
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      show-hidden = true;
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      recursive-search = "always";
+      search-filter-time-type = "last_modified";
+      show-directory-item-counts = "always";
+      show-image-thumbnails = "always";
+    };
+
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
+  };
+
+  impermanence.directories =
+    lib.mkIf (builtins.elem pkgs.gnome-keyring osConfig.environment.systemPackages)
+      [ ".local/share/keyrings" ];
+}
