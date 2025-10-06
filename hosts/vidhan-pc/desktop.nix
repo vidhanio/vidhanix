8e02ec86ce2ce7cc9cccd4dba209dddc91a8127b
Vidhan Bhatt <me@vidhan.io>
2025-10-05 21:41:33 -0400

    chore: rebuild system

--- hosts/vidhan-pc/desktop.nix
+++ hosts/vidhan-pc/desktop.nix
@@ -1,7 +1,13 @@
+{ pkgs, ... }:
 {
   services = {
     displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+  ];
 
   programs.dconf = {
     enable = true;
