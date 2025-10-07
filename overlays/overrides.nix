cd771db2de71d0f92324b850cd91bb7fa4bbac3e
Vidhan Bhatt <me@vidhan.io>
2025-10-07 18:38:47 -0400

    add broken overlay

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -5,4 +5,11 @@ final: prev: {
       cp $src/share/autostart/solaar.desktop $out/share/autostart/solaar.desktop
     '';
   });
+
+  # NixOS/nixpkgs#449396
+  qgnomeplatform = prev.qgnomeplatform.overrideAttrs (previousAttrs: {
+    cmakeFlags = (previousAttrs.cmakeFlags or [ ]) ++ [
+      "-DCMAKE_POLICY_VERSION_MINIMUM=3.10"
+    ];
+  });
 }
