2d37ed1acc137f813029e4f6e0e36ba46b35a55e
Vidhan Bhatt <me@vidhan.io>
2025-10-16 01:06:21 -0400

    add darwin fixes

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -12,6 +12,7 @@
         padding = 10;
       in
       {
+        macos-option-as-alt = "left";
         custom-shader = [ "${pkgs.ghostty-shader-playground}/share/shaders/cursor_smear.glsl" ];
         window-padding-x = padding;
         window-padding-y = padding;
