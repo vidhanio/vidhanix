db2c2332484ffcc6d5b64b8a5c2febf540bc3b9d
Vidhan Bhatt <me@vidhan.io>
2025-10-18 11:20:17 -0400

    no flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org/"
-      "https://install.determinate.systems"
-      "https://cache.flakehub.com/"
-      "https://nix-community.cachix.org/"
-    ];
-
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
   inputs = {
     self.submodules = true;
 
