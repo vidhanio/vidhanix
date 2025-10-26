104595e1d2a6f8312c6201124c73b3f61e8d3a5d
Vidhan Bhatt <me@vidhan.io>
2025-10-25 20:03:17 -0400

    update

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -1,1 +1,11 @@
-final: prev: { }
+final: prev: {
+  qgnomeplatform-qt6 = prev.qgnomeplatform-qt6.overrideAttrs (previousAttrs: {
+    patches = previousAttrs.patches or [ ] ++ [
+      (final.fetchurl {
+        # https://github.com/NixOS/nixpkgs/pull/455370
+        url = "https://raw.githubusercontent.com/nekowinston/nixpkgs/refs/heads/qgnomeplatform/fix-qt6.10-build/pkgs/development/libraries/qgnomeplatform/qt6_10.patch";
+        hash = "sha256-PmKqTflTlHwmLTxwCVUVPZubG+L4jRZZaH1CGQFH+bw=";
+      })
+    ];
+  });
+}
