69a79efafedf088e934ebdfcf48d379e65b9854e
Vidhan Bhatt <me@vidhan.io>
2025-10-31 23:27:05 -0400

    update patch url

--- overlays/patches.nix
+++ overlays/patches.nix
@@ -3,7 +3,7 @@ final: prev: {
     patches = previousAttrs.patches or [ ] ++ [
       (final.fetchurl {
         # https://github.com/NixOS/nixpkgs/pull/455370
-        url = "https://raw.githubusercontent.com/nekowinston/nixpkgs/refs/heads/qgnomeplatform/fix-qt6.10-build/pkgs/development/libraries/qgnomeplatform/qt6_10.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/master/pkgs/development/libraries/qgnomeplatform/qt6_10.patch";
         hash = "sha256-PmKqTflTlHwmLTxwCVUVPZubG+L4jRZZaH1CGQFH+bw=";
       })
     ];
