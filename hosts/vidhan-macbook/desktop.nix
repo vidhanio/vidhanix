224ce332b146dc72998056c9f4a413fbdf88f947
Vidhan Bhatt <me@vidhan.io>
2025-11-02 18:28:20 -0500

    add persist and kbs

--- hosts/vidhan-macbook/desktop.nix
+++ hosts/vidhan-macbook/desktop.nix
@@ -9,6 +9,22 @@
         "org/gnome/mutter" = {
           experimental-features = [ "scale-monitor-framebuffer" ];
         };
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          custom-keybindings = [
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/kbbrightnessdown/"
+            "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/kbbrightnessup/"
+          ];
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/kbbrightnessdown" = {
+          name = "Keyboard brightness down";
+          command = "gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Keyboard.StepDown";
+          binding = "<Shift>MonBrightnessDown";
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/kbbrightnessup" = {
+          name = "Keyboard brightness up";
+          command = "gdbus call --session --dest org.gnome.SettingsDaemon.Power --object-path /org/gnome/SettingsDaemon/Power --method org.gnome.SettingsDaemon.Power.Keyboard.StepUp";
+          binding = "<Shift>MonBrightnessUp";
+        };
       };
     }
   ];
