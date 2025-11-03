3934ea984ee05801a817b32eff505119c8f10f1e
Vidhan Bhatt <me@vidhan.io>
2025-11-02 23:38:03 -0500

    add subs to flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,16 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org/"
+      "https://nixos-apple-silicon.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
