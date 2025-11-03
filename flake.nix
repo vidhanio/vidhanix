ed30f1830f4bc29385ffa66a75d5a7e0d46ac890
Vidhan Bhatt <me@vidhan.io>
2025-11-02 23:43:30 -0500

    add fh cache

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
     extra-substituters = [
       "https://nix-community.cachix.org/"
       "https://nixos-apple-silicon.cachix.org"
+      "https://install.determinate.systems"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
     ];
   };
 
