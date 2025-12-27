999631329a6fdb52d48a440aeb3721a2bd4da3db
Vidhan Bhatt <me@vidhan.io>
2025-12-27 14:43:52 -0500

    clean

--- config/home/programs/bat.nix
+++
@@ -1,7 +0,0 @@
-{ lib, config, ... }:
-let
-  cfg = config.programs.bat;
-in
-lib.mkIf cfg.enable {
-  home.shellAliases.cat = "bat";
-}
--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -1,14 +1,52 @@
-{ withSystem, ... }:
 {
   flake.modules.homeManager.default =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
-      programs.vscode = withSystem pkgs.stdenvNoCC.hostPlatform.system {
+      programs.vscode = {
+        enable = true;
         package = pkgs.vscode-insiders;
-      };
 
-      persist.directories = [
-        ".config/Code - Insiders/User/globalStorage"
-      ];
+        mutableExtensionsDir = false;
+
+        profiles.default = {
+          userSettings = lib.importJSON ./settings.json;
+
+          extensions =
+            with pkgs.vscode-marketplace;
+            [
+              bmalehorn.vscode-fish
+              bradlc.vscode-tailwindcss
+              charliermarsh.ruff
+              codezombiech.gitignore
+              github.copilot
+              github.copilot-chat
+              jnoortheen.nix-ide
+              mkhl.direnv
+              mkhl.shfmt
+              ms-python.python
+              ms-python.vscode-pylance
+              myriad-dreamin.tinymist
+              pkief.material-icon-theme
+              pkief.material-product-icons
+              redhat.vscode-yaml
+              rust-lang.rust-analyzer
+              timonwong.shellcheck
+              tombi-toml.tombi
+              tomoki1207.pdf
+              ultram4rine.vscode-choosealicense
+              vscodevim.vim
+              wakatime.vscode-wakatime
+              ibecker.treefmt-vscode
+            ]
+            ++ lib.optionals pkgs.stdenv.hostPlatform.isx86_64 [
+              # https://github.com/redhat-developer/vscode-xml/pull/1112
+              redhat.vscode-xml
+            ];
+        };
+
+        persist.directories = [
+          ".config/Code - Insiders/User/globalStorage"
+        ];
+      };
     };
 }
