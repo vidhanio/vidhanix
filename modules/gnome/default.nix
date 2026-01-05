7f34be60dd9e57892638c9e90bf5d9aa50d4b269
Vidhan Bhatt <me@vidhan.io>
2026-01-04 21:13:33 -0500

    fix: remove gnome pre-installs (#7)

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -9,6 +9,25 @@
 
       environment.gnome.excludePackages = with pkgs; [
         gnome-tour
+        decibels
+        epiphany
+        gnome-text-editor
+        gnome-calculator
+        gnome-calendar
+        gnome-clocks
+        gnome-console
+        gnome-contacts
+        gnome-logs
+        gnome-maps
+        gnome-music
+        gnome-weather
+        loupe
+        papers
+        gnome-connections
+        showtime
+        simple-scan
+        snapshot
+        yelp
       ];
     };
 }
