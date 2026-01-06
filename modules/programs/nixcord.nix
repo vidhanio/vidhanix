79dbe52de881312cac2eab5f5949df940c88c77a
Vidhan Bhatt <me@vidhan.io>
2026-01-06 12:45:25 -0500

    fix: remove vesktop patch

--- modules/programs/nixcord.nix
+++ modules/programs/nixcord.nix
@@ -2,64 +2,42 @@
 {
   flake-file.inputs.nixcord.url = "github:FlameFlag/nixcord";
 
-  flake.modules.homeManager.default =
-    { pkgs, ... }:
-    {
-      imports = [ inputs.nixcord.homeModules.default ];
+  flake.modules.homeManager.default = {
+    imports = [ inputs.nixcord.homeModules.default ];
 
-      programs.nixcord = {
+    programs.nixcord = {
+      enable = true;
+      discord.enable = lib.mkDefault false;
+      vesktop = {
         enable = true;
-        discord.enable = lib.mkDefault false;
-        vesktop = {
-          enable = true;
-          # https://github.com/NixOS/nixpkgs/pull/476514
-          package = pkgs.vesktop.overrideAttrs {
-            preBuild = lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-              cp -r ${pkgs.electron}/libexec/electron electron-dist
-              chmod -R u+w electron-dist
-            '';
-
-            buildPhase = ''
-              runHook preBuild
-
-              pnpm build --standalone
-              pnpm exec electron-builder \
-                --dir \
-                -c.asarUnpack="**/*.node" \
-                -c.electronDist=electron-dist \
-                -c.electronVersion=${pkgs.electron.version}
-
-              runHook postBuild
-            '';
-          };
-          settings = {
-            discordBranch = "canary";
-            minimizeToTray = true;
-            arRPC = true;
-            hardwareAcceleration = true;
-            hardwareVideoAcceleration = true;
-            customTitleBar = false;
-            disableMinSize = true;
-          };
-          state = {
-            firstLaunch = false;
-            maximized = true;
-          };
+        settings = {
+          discordBranch = "canary";
+          minimizeToTray = true;
+          arRPC = true;
+          hardwareAcceleration = true;
+          hardwareVideoAcceleration = true;
+          customTitleBar = false;
+          disableMinSize = true;
         };
-        quickCss = ''
-          @import url(https://codeberg.org/ridge/Discord-Adblock/raw/branch/main/discord-adblock.css);
-        '';
-        config = {
-          useQuickCss = true;
-          plugins = {
-            spotifyCrack.enable = true;
-            fakeNitro.enable = true;
-            youtubeAdblock.enable = true;
-            ClearURLs.enable = true;
-          };
+        state = {
+          firstLaunch = false;
+          maximized = true;
         };
       };
-
-      persist.directories = [ ".config/vesktop/sessionData/Local Storage" ];
+      quickCss = ''
+        @import url(https://codeberg.org/ridge/Discord-Adblock/raw/branch/main/discord-adblock.css);
+      '';
+      config = {
+        useQuickCss = true;
+        plugins = {
+          spotifyCrack.enable = true;
+          fakeNitro.enable = true;
+          youtubeAdblock.enable = true;
+          ClearURLs.enable = true;
+        };
+      };
     };
+
+    persist.directories = [ ".config/vesktop/sessionData/Local Storage" ];
+  };
 }
