832d051f882d112af864cf93a9fdb79f91859058
Vidhan Bhatt <me@vidhan.io>
2026-01-05 20:40:43 -0500

    comma

---
+++ modules/nix/no-channel.nix
@@ -0,0 +1,5 @@
+{
+  flake.modules.nixos.default = {
+    nix.channel.enable = false;
+  };
+}
--- modules/programs/comma.nix
+++ modules/programs/comma.nix
@@ -2,11 +2,13 @@
 {
   flake-file.inputs.nix-index-database.url = "github:nix-community/nix-index-database";
 
-  flake.modules.nixos.default = {
+  flake.modules.homeManager.default = {
     imports = [
-      inputs.nix-index-database.nixosModules.default
+      inputs.nix-index-database.homeModules.default
     ];
 
     programs.nix-index-database.comma.enable = true;
+
+    persist.files = [ ".local/state/comma/choices" ];
   };
 }
