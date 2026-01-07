af98dfd98228b8c4e86d7fe58ff30c3512b28a54
Vidhan Bhatt <me@vidhan.io>
2026-01-06 19:24:34 -0500

    fix: get rid of treefmt xmllint patch (numtide/treefmt-nix#459)

--- modules/treefmt.nix
+++ modules/treefmt.nix
@@ -1,4 +1,4 @@
-{ lib, inputs, ... }:
+{ inputs, ... }:
 {
   imports = [
     inputs.treefmt-nix.flakeModule
@@ -6,51 +6,31 @@
 
   flake-file.inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
 
-  perSystem =
-    { pkgs, config, ... }:
-    {
-      treefmt = {
-        programs = {
-          nixfmt.enable = true;
-          statix.enable = true;
-          deadnix.enable = true;
+  perSystem = {
+    treefmt = {
+      programs = {
+        nixfmt.enable = true;
+        statix.enable = true;
+        deadnix.enable = true;
 
-          shfmt.enable = true;
-          shellcheck.enable = true;
+        shfmt.enable = true;
+        shellcheck.enable = true;
 
-          actionlint.enable = true;
-          yamlfmt.enable = true;
+        actionlint.enable = true;
+        yamlfmt.enable = true;
 
-          prettier.enable = true;
+        prettier.enable = true;
 
-          xmllint.enable = true;
-        };
-
-        settings = {
-          on-unmatched = "fatal";
-
-          # https://github.com/numtide/treefmt-nix/pull/459
-          formatter.xmllint.command = lib.mkForce (
-            pkgs.writeShellApplication {
-              name = "xmllint-wrapper";
-              text = ''
-                temp=$(mktemp)
-                trap 'rm "$temp"' EXIT
-                for file in "$@"; do
-                  ${lib.getExe' config.treefmt.programs.xmllint.package "xmllint"} --format "$file" --output "$temp"
-                  if ! cmp -s "$file" "$temp"; then
-                    cp "$temp" "$file"
-                  fi
-                done
-              '';
-            }
-          );
-        };
+        xmllint.enable = true;
       };
 
-      pre-commit.settings.hooks.treefmt = {
-        enable = true;
-        pass_filenames = false;
-      };
+      settings.on-unmatched = "fatal";
+
     };
+
+    pre-commit.settings.hooks.treefmt = {
+      enable = true;
+      pass_filenames = false;
+    };
+  };
 }
