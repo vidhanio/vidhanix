84b7e268a2fa65826fc67d04607f2f3899bc152d
Vidhan Bhatt <me@vidhan.io>
2026-01-08 16:48:13 -0500

    feat(fonts): google-sans-flex

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,6 +1,32 @@
+{ withSystem, ... }:
 {
   flake-file.inputs.vidhan-fonts = {
     url = "git+ssh://git@github.com/vidhanio/fonts";
     flake = false;
   };
+
+  flake.modules.nixos.default =
+    { pkgs, config, ... }:
+    let
+      cfg = config.stylix;
+    in
+    {
+      stylix = {
+        fonts = {
+          monospace = {
+            package = withSystem pkgs.stdenv.hostPlatform.system (
+              { self', ... }: self'.packages.berkeley-mono-variable
+            );
+            name = "Berkeley Mono Variable";
+          };
+          serif = cfg.fonts.monospace;
+          sansSerif = {
+            package = withSystem pkgs.stdenv.hostPlatform.system (
+              { self', ... }: self'.packages.google-sans-flex
+            );
+            name = "Google Sans Flex";
+          };
+        };
+      };
+    };
 }
