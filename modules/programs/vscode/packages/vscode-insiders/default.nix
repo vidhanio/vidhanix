c8244b564a628e3612f0db3f58225f7880efe083
Vidhan Bhatt <me@vidhan.io>
2026-01-10 15:10:01 -0500

    chore(pkgs): bump

--- modules/programs/vscode/packages/vscode-insiders/default.nix
+++ modules/programs/vscode/packages/vscode-insiders/default.nix
@@ -1,22 +1,22 @@
 let
   pkg =
     {
-      stdenvNoCC,
+      stdenv,
       lib,
       vscode,
       fetchurl,
     }:
     let
-      inherit (stdenvNoCC.hostPlatform) system;
+      inherit (stdenv.hostPlatform) system;
 
       platforms = {
         "x86_64-linux" = {
           os = "linux-x64";
-          hash = "sha256-Zhl9pSscc+QaFrN0joKWvNxKlx9CCjcbFDy8O0/IScc=";
+          hash = "sha256-wqjW/kKrqZN9TSgpSd8cz6+jbBelyIDJy8uKfVdzXbU=";
         };
         "aarch64-linux" = {
           os = "linux-arm64";
-          hash = "sha256-84GVPiS2vBATOD/nfNu1db/oawSqJsOqEosIvIjD5bQ=";
+          hash = "sha256-8gpIy2OiD0cZxmLepMSXmBp/Dt/jjASlNs+swMXm6nI=";
         };
       };
 
@@ -24,8 +24,8 @@ let
     in
     (vscode.override { isInsiders = true; }).overrideAttrs (
       finalAttrs: prevAttrs: {
-        version = "1.108.0-insider-2026-01-07";
-        commit = "3d862df21449d5ead0b0b1fbe6d43eb05490cb1c";
+        version = "1.109.0-insider-2026-01-09";
+        commit = "7c62052af606ba507cbb8ee90b0c22957bb175e7";
 
         src = fetchurl {
           name = "vscode-insiders-${finalAttrs.commit}-${os}.tar.gz";
