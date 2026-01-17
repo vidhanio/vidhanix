29e02fdeba1c33d7b9372920964e94b580146002
Vidhan Bhatt <me@vidhan.io>
2026-01-16 22:24:44 -0500

    feat: some upgrades

--- modules/programs/vscode/packages/vscode-insiders/default.nix
+++ modules/programs/vscode/packages/vscode-insiders/default.nix
@@ -5,7 +5,6 @@ let
       lib,
       vscode,
       fetchurl,
-      ...
     }@args:
     let
       inherit (stdenv.hostPlatform) system;
@@ -13,19 +12,34 @@ let
       platforms = {
         "x86_64-linux" = {
           os = "linux-x64";
-          hash = "sha256-pmWRSa/tPwIrYr+Yg3bsXdBxZHO8WWxx2nh992CImPM=";
+          hash = "sha256-upLOEwcNtmn5/JKDBWRxHHcKumBupAVFAvb/pfpfiSs=";
         };
         "aarch64-linux" = {
           os = "linux-arm64";
-          hash = "sha256-GNY3O6P9VkGzNAxk7svkp1tkIIYc01QkkGx43b6V39Y=";
+          hash = "sha256-ppwTHdWdGDBcf9s0oA0hUc7/7NysZykfpOlfl6bkfxo=";
         };
       };
 
       inherit (platforms.${system} or (throw "Unsupported system: ${system}")) os hash;
     in
-    (vscode.override (
+    (vscode.override { isInsiders = true; }).overrideAttrs (
+      finalAttrs: prevAttrs:
       {
-        isInsiders = true;
+        version = "1.109.0-insider-2026-01-16";
+        commit = "f46196091d2fa0069d229cb638869b4ab95392cf";
+
+        src = fetchurl {
+          name = "vscode-insiders-${finalAttrs.commit}-${os}.tar.gz";
+          url = "https://update.code.visualstudio.com/commit:${finalAttrs.commit}/${os}/insider";
+          inherit hash;
+        };
+
+        passthru.updateScript = ./update.sh;
+
+        meta = prevAttrs.meta // {
+          mainProgram = "code-insiders";
+          platforms = lib.attrNames platforms;
+        };
       }
       // (lib.removeAttrs args [
         "stdenv"
@@ -33,26 +47,7 @@ let
         "vscode"
         "fetchurl"
       ])
-    )).overrideAttrs
-      (
-        finalAttrs: prevAttrs: {
-          version = "1.109.0-insider-2026-01-16";
-          commit = "03f79ac30bd46fc92f3d2be4cd92906e7a375761";
-
-          src = fetchurl {
-            name = "vscode-insiders-${finalAttrs.commit}-${os}.tar.gz";
-            url = "https://update.code.visualstudio.com/commit:${finalAttrs.commit}/${os}/insider";
-            inherit hash;
-          };
-
-          passthru.updateScript = ./update.sh;
-
-          meta = prevAttrs.meta // {
-            mainProgram = "code-insiders";
-            platforms = lib.attrNames platforms;
-          };
-        }
-      );
+    );
 in
 {
   perSystem =
