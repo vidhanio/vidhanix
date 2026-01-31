1997802a7db467180d6985d8637108fb9f07c610
Vidhan Bhatt <me@vidhan.io>
2026-01-30 23:27:17 -0500

    feat: more noctalia changes

--- modules/desktop/hyprland/settings/default.nix
+++ modules/desktop/hyprland/settings/default.nix
@@ -30,8 +30,8 @@
 
           general = {
             border_size = 2;
-            gaps_in = 4;
-            gaps_out = 8;
+            gaps_in = 8;
+            gaps_out = 16;
           };
 
           decoration = {
--- modules/users/vidhanio.nix
+++ modules/users/vidhanio.nix
@@ -2,9 +2,15 @@
   users.vidhanio = {
     fullName = "Vidhan Bhatt";
     email = "me@vidhan.io";
-    module = {
-      programs.gh.username = "vidhanio";
-    };
+    module =
+      { pkgs, ... }:
+      {
+        programs.gh.username = "vidhanio";
+        home.file.".face".source = pkgs.fetchurl {
+          url = "https://github.com/vidhanio.png";
+          hash = "sha256-ApFyRcwZJ6V7wBNpQwYwGzzw+R17bZe1NGV0ikOzjOo=";
+        };
+      };
   };
   flake.modules.nixos.default = {
     users.users.vidhanio.enable = true;
