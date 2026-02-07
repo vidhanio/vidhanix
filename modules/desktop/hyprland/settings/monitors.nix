e3cac3060435b741ccc1d4c1855d9a37af29c08b
Vidhan Bhatt <me@vidhan.io>
2026-02-07 13:53:17 -0500

    feat: bunch of stuff

--- modules/desktop/hyprland/settings/monitors.nix
+++ modules/desktop/hyprland/settings/monitors.nix
@@ -14,31 +14,14 @@
     vidhan-pc.homeModule = {
       wayland.windowManager.hyprland.settings = {
         monitor = [
-          #          ┌─────┐
-          #          │     │
-          #          │     │
-          # ┌────────┤     │
-          # │        │     │
-          # └────────┴─────┘
-          "DP-1, 2560x1440@300, 0x0, 1"
-          "HDMI-A-1, 2560x1080@60, 2560x-1120, 1, transform, 3"
-        ];
-
-        workspace = [
-          # workspaces
-          "1, monitor:DP-1, default:true"
-          "2, monitor:HDMI-A-1, default:true"
+          "DP-1, 2560x1440@300, auto, 1"
         ];
       };
     };
     vidhan-macbook.homeModule = {
       wayland.windowManager.hyprland.settings = {
         monitor = [
-          "eDP-1, preferred, 0x0, 1.6"
-        ];
-
-        workspace = [
-          "1, monitor:eDP-1, default:true"
+          "eDP-1, preferred, auto, 1.6"
         ];
       };
     };
