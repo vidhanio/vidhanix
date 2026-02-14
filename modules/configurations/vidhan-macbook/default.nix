226a038027061f9f6bf31aa8847967ae2d9e0a56
Vidhan Bhatt <me@vidhan.io>
2026-02-14 12:07:20 -0500

    feat: macbook setup

--- modules/configurations/vidhan-macbook/default.nix
+++ modules/configurations/vidhan-macbook/default.nix
@@ -1,8 +1,12 @@
 { config, ... }:
 {
-  configurations.vidhan-macbook.module = {
-    imports = with config.flake.modules.nixos; [ macbook ];
-    nixpkgs.hostPlatform = "aarch64-linux";
-    system.stateVersion = "26.05";
+  configurations.vidhan-macbook = {
+    users = [ "vidhanio" ];
+    publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINrlsMqmLKW+8+MyHndMWNZXk86Oo0Ik8wPs3v1Nx7ZR root@vidhan-macbook";
+    module = {
+      imports = with config.flake.modules.nixos; [ macbook ];
+      nixpkgs.hostPlatform = "aarch64-linux";
+      system.stateVersion = "26.05";
+    };
   };
 }
