129b0fc5532f50925e9a28ccd0ccf76113aad78f
Vidhan Bhatt <me@vidhan.io>
2026-02-14 21:31:10 -0500

    chore: bump

--- flake.nix
+++ flake.nix
@@ -7,13 +7,11 @@
     allow-import-from-derivation = false;
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
       "https://nixos-apple-silicon.cachix.org"
       "https://cache.numtide.com"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
@@ -34,7 +32,6 @@
     };
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
     home-manager.url = "github:nix-community/home-manager";
-    hyprland.url = "github:hyprwm/Hyprland";
     impermanence.url = "github:nix-community/impermanence";
     import-tree.url = "github:vic/import-tree";
     llm-agents.url = "github:numtide/llm-agents.nix";
--- modules/programs/vscode/packages/vscode-insiders/default.nix
+++ modules/programs/vscode/packages/vscode-insiders/default.nix
@@ -13,11 +13,11 @@ let
       platforms = {
         "x86_64-linux" = {
           os = "linux-x64";
-          hash = "sha256-C9KgqUpAiwT5Q4ASslaNkKOIUYpIcyrN6RVSFWxSzwc=";
+          hash = "sha256-dxrix9xhM5/y7XlKoE6KGN+pW7KF7fu3bZgsAQPDFqs=";
         };
         "aarch64-linux" = {
           os = "linux-arm64";
-          hash = "sha256-3ZHNoCA5Ur+8YGnIZrfQ+ItrtprqE4j8Uqbo2oTO/Bs=";
+          hash = "sha256-kbPVccpDh3Df4ioAuw11hyEiddv5NXNjaoMhH1uYWY4=";
         };
       };
 
@@ -36,8 +36,8 @@ let
     )).overrideAttrs
       (
         finalAttrs: prevAttrs: {
-          version = "1.110.0-insider-2026-02-12";
-          commit = "cc318781a13d6869a36573f9de9eabacc9fad49f";
+          version = "1.110.0-insider-2026-02-13";
+          commit = "0366405821955682155a4507a122611c742df3fa";
 
           src = fetchurl {
             name = "vscode-insiders-${finalAttrs.commit}-${os}.tar.gz";
