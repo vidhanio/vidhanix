228bc7d9495d71ffd666b1ae12da760f962241e9
Vidhan Bhatt <me@vidhan.io>
2026-02-20 11:45:41 -0500

    feat: add lazygit and update hyprland apps

--- modules/desktop/hyprland/settings/apps.nix
+++ modules/desktop/hyprland/settings/apps.nix
@@ -3,21 +3,11 @@
     homeManager.default = {
       wayland.windowManager.hyprland.settings = {
         exec-once = map (app: "uwsm app -- ${app}") [
-          "code-insiders"
+          "ghostty"
           "helium"
 
           "spotify"
           "vesktop"
-
-          "1password --silent"
-        ];
-
-        windowrule = [
-          "match:class code-insiders, workspace 1 silent"
-          "match:class helium, workspace 1 silent"
-
-          "match:class Spotify, workspace 2 silent"
-          "match:class vesktop, workspace 2 silent"
         ];
       };
     };
