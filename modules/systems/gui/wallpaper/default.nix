f169ff46f76155bd06af72469362fadaa583f18e
Vidhan Bhatt <me@vidhan.io>
2026-02-20 19:17:04 -0500

    feat: nokia wallpaper

--- modules/systems/gui/wallpaper/default.nix
+++ modules/systems/gui/wallpaper/default.nix
@@ -6,26 +6,13 @@
       {
         stylix = {
           image =
-            let
-              svg = pkgs.replaceVars ./wallpaper.svg (
-                with config.lib.stylix.colors.withHashtag;
-                {
-                  bg = base01;
-
-                  purple = base0E;
-                  black = base01;
-                  red = base08;
-
-                  ball = base04;
-
-                  claws = base07;
-                  teeth = base07;
-                  eye = base07;
-                }
-              );
-            in
-            pkgs.runCommandLocal "wallpaper.png" { inherit svg; } ''
-              ${lib.getExe pkgs.librsvg} $svg -o $out
+            with config.lib.stylix.colors.withHashtag;
+            pkgs.runCommandLocal "wallpaper.png" { } ''
+              ${lib.getExe' pkgs.imagemagick "magick"} \
+                ${./nokia.png} \
+                -fuzz 50% -fill "${base01}" -opaque black \
+                -fuzz 50% -fill "${base07}" -opaque white \
+                $out
             '';
         };
       };
