672fd45eac8e4fc73a439918ad15e79514eb979d
Vidhan Bhatt <me@vidhan.io>
2026-02-27 18:24:09 -0500

    feat: stuff

--- modules/systems/users/vidhanio.nix
+++ modules/systems/users/vidhanio.nix
@@ -3,8 +3,6 @@
     fullName = "Vidhan Bhatt";
     email = "me@vidhan.io";
     publicKeys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxMGko3NUtTtMB7pfDE1VYnTy1OR1fsLaGpVp9FaKtv vidhanio@vidhan-pc"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpEHUbyfwBLGJqsrZLO8xDpldmg655DPYLGNOJUJfHM vidhanio@vidhan-macbook"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcNbwiEfw2GG4G//eWdtjyuv4S7vlkHuB9Z1INIfDwE vidhanio@vidhan-iphone"
     ];
     module = {
