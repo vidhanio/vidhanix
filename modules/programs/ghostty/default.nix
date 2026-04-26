34f87ce8419badc7f79c91c222860734f942ecf4
Vidhan Bhatt <me@vidhan.io>
2026-04-26 10:33:52 -0400

    chore: upd

--- modules/programs/ghostty/default.nix
+++ modules/programs/ghostty/default.nix
@@ -1,29 +1,27 @@
 {
-  config,
-  ...
-}:
-{
-  flake.modules.homeManager.default = {
-    programs.ghostty = {
-      enable = true;
-      settings =
-        let
-          padding = 10;
-        in
-        {
-          window-padding-x = padding;
-          window-padding-y = padding;
-        };
+  flake.modules.homeManager.default =
+    { config, ... }:
+    {
+      programs.ghostty = {
+        enable = true;
+        settings =
+          let
+            padding = 10;
+          in
+          {
+            window-padding-x = padding;
+            window-padding-y = padding;
+          };
+      };
+
+      xdg.autostart.entries = [
+        "${config.programs.ghostty.package}/share/applications/com.mitchellh.ghostty.desktop"
+      ];
+
+      hyprland.autostartWorkspaces.ghostty = 1;
+
+      wayland.windowManager.hyprland.settings.bind = [
+        "SUPER, T, exec, uwsm app -- ghostty"
+      ];
     };
-
-    xdg.autostart.entries = [
-      "${config.programs.ghostty.package}/share/applications/com.mitchellh.ghostty.desktop"
-    ];
-
-    hyprland.autostartWorkspaces.ghostty = 1;
-
-    wayland.windowManager.hyprland.settings.bind = [
-      "SUPER, T, exec, uwsm app -- ghostty"
-    ];
-  };
 }
