a8aa00cffbfa7a606649c7a538f24973862c2620
Vidhan Bhatt <me@vidhan.io>
2026-07-29 01:57:05 -0400

    feat: comma improvements

--- modules/programs/comma.nix
+++ modules/programs/comma.nix
@@ -1,9 +1,23 @@
-{ inputs, ... }:
+{ inputs, withSystem, ... }:
 {
   flake-file.inputs.nix-index-database.url = "github:nix-community/nix-index-database";
 
   flake.modules.homeManager.default =
     { pkgs, lib, ... }:
+    let
+      binNames =
+        pkgs.runCommand "comma-fish-bin-names"
+          {
+            nativeBuildInputs = [
+              (withSystem pkgs.stdenv.hostPlatform.system (
+                { inputs', ... }: inputs'.nix-index-database.packages.nix-index-with-db
+              ))
+            ];
+          }
+          ''
+            nix-locate --at-root "/bin/" | sed -E 's#.*/##' | sort -u > $out
+          '';
+    in
     {
       imports = [
         inputs.nix-index-database.homeModules.default
@@ -11,10 +25,20 @@
 
       programs.nix-index-database.comma.enable = true;
 
+      programs.fish.completions = {
+        comma = ''
+          comma --print-completions fish 2>/dev/null | source
+
+          complete -c comma -n "__fish_comma_needs_command" -a "(cat ${binNames})"
+        '';
+
+        "," = "complete -c , -w comma";
+      };
+
       programs.noctalia.settings.shell.launcher.dmenu.entry.comma =
         let
           picker = pkgs.writeShellScript "comma-noctalia-picker" ''
-            choice=$(sed 's/\.out$//' | noctalia dmenu -p "Pick a package")
+            choice=$(sed 's/\.out$//' | sort | noctalia dmenu -p "Pick a package")
             if [[ -n "$choice" ]]; then
               printf '%s.out\n' "$choice"
             fi
