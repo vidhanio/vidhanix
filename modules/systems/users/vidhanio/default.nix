bbf58da69baa90877450202c2eaf30d99b0c15fd
Vidhan Bhatt <me@vidhan.io>
2026-08-18 02:12:07 -0400

    feat(users): use local face image

--- modules/systems/users/vidhanio/default.nix
+++ modules/systems/users/vidhanio/default.nix
@@ -1,13 +1,3 @@
-{ withSystem, ... }:
-let
-  face = withSystem "x86_64-linux" (
-    { pkgs, ... }:
-    pkgs.fetchurl {
-      url = "https://github.com/vidhanio.png";
-      hash = "sha256-EKG8tBhhWgIiuBsYYoVPObzSdb9tZS+mqwon72h+AD8=";
-    }
-  );
-in
 {
   flake.aspects =
     { aspects, ... }:
@@ -24,6 +14,6 @@ in
     publicKeys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcNbwiEfw2GG4G//eWdtjyuv4S7vlkHuB9Z1INIfDwE vidhanio@vidhan-iphone"
     ];
-    inherit face;
+    face = ./face.png;
   };
 }
