dcaf45a0e258b13dab0cff07f882252e7f03b88f
Vidhan Bhatt <me@vidhan.io>
2026-08-19 23:25:06 -0400

    fix: override fex to 2608 via nixpkgs #554106

--- modules/flake/nixpkgs.nix
+++ modules/flake/nixpkgs.nix
@@ -8,6 +8,17 @@
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
         config.allowUnfree = true;
+        # TODO: drop once https://github.com/NixOS/nixpkgs/pull/554106 lands (fex 2605 -> 2608)
+        overlays = [
+          (_final: prev: {
+            fex = prev.fex.overrideAttrs (old: {
+              version = "2608";
+              src = old.src.override {
+                hash = "sha256-2NdkQpzqDkM/fEW8QYS05KU3JPJeLw4gliryqdOJ3vE=";
+              };
+            });
+          })
+        ];
       };
     };
 }
