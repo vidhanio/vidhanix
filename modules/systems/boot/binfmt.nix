bf4d3a5a34eaf25a06ab5def8ee0338b283d4eed
Vidhan Bhatt <me@vidhan.io>
2026-08-19 21:08:05 -0400

    feat: enable binfmt for non-native architectures

---
+++ modules/systems/boot/binfmt.nix
@@ -0,0 +1,18 @@
+{ lib, ... }:
+{
+  flake.aspects.boot = {
+    nixos =
+      { pkgs, ... }:
+      {
+        # make binfmt available for non-native architectures
+        boot.binfmt.emulatedSystems =
+          let
+            mkSystemIfNot = target: lib.mkIf (pkgs.stdenv.hostPlatform.system != target) [ target ];
+          in
+          lib.mkMerge [
+            (mkSystemIfNot "aarch64-linux")
+            (mkSystemIfNot "x86_64-linux")
+          ];
+      };
+  };
+}
