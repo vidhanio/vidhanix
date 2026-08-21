1e2f4e938a0274040fcb7513e567551a595d74dd
Vidhan Bhatt <me@vidhan.io>
2026-08-21 17:12:15 -0400

    feat(stylix): add nixos light and dark specialisations

--- modules/systems/gui/stylix/default.nix
+++ modules/systems/gui/stylix/default.nix
@@ -1,4 +1,26 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
+let
+  schemes = {
+    dark = {
+      polarity = "dark";
+      base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-mocha.yaml";
+    };
+    light = {
+      polarity = "light";
+      base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-latte.yaml";
+    };
+  };
+
+  # NixOS specialisations merge their configuration over the base at equal
+  # priority, so the overriding fields need mkForce. Harmless at the Home
+  # Manager level, where nothing else defines them.
+  specialisation = lib.mapAttrs (_: scheme: {
+    configuration.stylix = {
+      polarity = lib.mkForce scheme.polarity;
+      base16Scheme = lib.mkForce scheme.base16Scheme;
+    };
+  }) schemes;
+in
 {
   flake-file.inputs = {
     stylix.url = "github:nix-community/stylix";
@@ -14,28 +36,16 @@
 
       stylix = {
         enable = true;
-        polarity = "dark";
-        base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-mocha.yaml";
-      };
+      }
+      // schemes.dark;
+
+      inherit specialisation;
     };
 
     homeManager = {
       stylix.targets.kde.enable = false;
 
-      specialisation = {
-        dark.configuration = {
-          stylix = {
-            polarity = "dark";
-            base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-mocha.yaml";
-          };
-        };
-        light.configuration = {
-          stylix = {
-            polarity = "light";
-            base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-latte.yaml";
-          };
-        };
-      };
+      inherit specialisation;
     };
   };
 }
