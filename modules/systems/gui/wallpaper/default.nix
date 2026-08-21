621e3a74ecfd11938ab7b58c47cca9b10dec5815
Vidhan Bhatt <me@vidhan.io>
2026-08-20 20:52:11 -0400

    feat(wallpaper): narrow accent gradient range

--- modules/systems/gui/wallpaper/default.nix
+++ modules/systems/gui/wallpaper/default.nix
@@ -9,18 +9,24 @@
       {
         stylix = {
           image = pkgs.runCommandLocal "wallpaper.png" { } ''
-            halfway=$(
-              ${lib.getExe' pkgs.gawk "awk"} 'BEGIN {
-                printf "rgb(%.6f%%,%.6f%%,%.6f%%)",
-                  sqrt((${colors."base00-dec-r"} ^ 2 + ${colors."base0D-dec-r"} ^ 2) / 2) * 100,
-                  sqrt((${colors."base00-dec-g"} ^ 2 + ${colors."base0D-dec-g"} ^ 2) / 2) * 100,
-                  sqrt((${colors."base00-dec-b"} ^ 2 + ${colors."base0D-dec-b"} ^ 2) / 2) * 100
-              }'
+            gradient=$(
+              ${lib.getExe' pkgs.gawk "awk"} '
+                function rms(a, b, weight) {
+                  return sqrt((1 - weight) * a ^ 2 + weight * b ^ 2) * 100
+                }
+                BEGIN {
+                  r0 = ${colors.base00-dec-r}; g0 = ${colors.base00-dec-g}; b0 = ${colors.base00-dec-b}
+                  rD = ${colors.base0D-dec-r}; gD = ${colors.base0D-dec-g}; bD = ${colors.base0D-dec-b}
+                  printf "rgb(%.6f%%,%.6f%%,%.6f%%),rgb(%.6f%%,%.6f%%,%.6f%%)",
+                    rms(r0, rD, 0.25), rms(g0, gD, 0.25), rms(b0, bD, 0.25),
+                    rms(r0, rD, 0.50), rms(g0, gD, 0.50), rms(b0, bD, 0.50)
+                }
+              '
             )
 
             ${lib.getExe' pkgs.imagemagick "magick"} \
               ${./iceman.png} \
-              +level-colors "${colors.withHashtag.base00},$halfway" \
+              +level-colors "$gradient" \
               -colorspace sRGB \
               $out
           '';
