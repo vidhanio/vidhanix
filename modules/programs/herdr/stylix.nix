8673d24e8c026d66f497c8fdb8eecde06cafa977
Vidhan Bhatt <me@vidhan.io>
2026-08-21 01:18:31 -0400

    fix(herdr): rebuild stylix theme mappings

--- modules/programs/herdr/stylix.nix
+++ modules/programs/herdr/stylix.nix
@@ -1,5 +1,4 @@
 {
-  # Vendored from github:vidhanio/stylix (vidhanio) modules/herdr/hm.nix.
   flake.aspects.herdr = {
     homeManager =
       { config, ... }:
@@ -7,23 +6,27 @@
         programs.herdr.settings.theme.custom = with config.lib.stylix.colors.withHashtag; {
           accent = base0D;
 
-          panel_bg = base00;
-          surface_dim = base01;
+          panel_bg = base01;
+          sidebar_bg = "reset";
+          active_row_bg = base00;
+          selection_bg = base02;
+
           surface0 = base02;
           surface1 = base03;
+          surface_dim = base00;
 
-          overlay0 = base03;
+          overlay0 = base04;
           overlay1 = base04;
-          subtext0 = base04;
           text = base05;
+          subtext0 = base04;
 
-          red = base08;
-          peach = base09;
-          yellow = base0A;
-          green = base0B;
-          teal = base0C;
-          blue = base0D;
           mauve = base0E;
+          green = base0B;
+          yellow = base0A;
+          red = base08;
+          blue = base0D;
+          teal = base0C;
+          peach = base09;
         };
       };
   };
