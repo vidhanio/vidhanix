25dac797ca9a893c27325cd0cbc91536ae09dd8e
Vidhan Bhatt <me@vidhan.io>
2026-08-21 01:11:58 -0400

    refactor(hyprland): configure screencopy through home manager

--- modules/systems/gui/hyprland/default.nix
+++ modules/systems/gui/hyprland/default.nix
@@ -13,17 +13,11 @@
         xdg.configFile."uwsm/env".source =
           "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
 
-        # make screenshare dialog not pop up multiple times
-        xdg.configFile."hypr/xdph.conf".text = ''
-          screencopy {
-            allow_token_by_default = true
-          }
-        '';
-
         wayland.windowManager.hyprland = {
           enable = true;
           # conflicts with UWSM
           systemd.enable = false;
+          xdph.settings.screencopy.allow_token_by_default = true;
         };
       };
   };
