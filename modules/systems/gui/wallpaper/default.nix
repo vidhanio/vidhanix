919b57aa6000e87bd40ac2fdaf6fbe843e8beb92
Vidhan Bhatt <me@vidhan.io>
2026-08-20 20:16:37 -0400

    feat(wallpaper): derive gradient from accent

--- modules/systems/gui/wallpaper/default.nix
+++ modules/systems/gui/wallpaper/default.nix
@@ -3,17 +3,25 @@
   flake.aspects.wallpaper = {
     nixos =
       { pkgs, config, ... }:
+      let
+        colors = config.lib.stylix.colors;
+        toFloat = value: builtins.fromJSON value;
+        sqrt = value: lib.foldl' (guess: _: (guess + value / guess) / 2.0) 1.0 (lib.range 1 12);
+        rms = a: b: sqrt ((a * a + b * b) / 2.0);
+        halfwayChannel =
+          channel: rms (toFloat colors."base00-dec-${channel}") (toFloat colors."base0D-dec-${channel}");
+        toPercent = value: "${toString (value * 100.0)}%";
+        halfway = "rgb(${toPercent (halfwayChannel "r")},${toPercent (halfwayChannel "g")},${toPercent (halfwayChannel "b")})";
+      in
       {
         stylix = {
-          image =
-            with config.lib.stylix.colors.withHashtag;
-            pkgs.runCommandLocal "wallpaper.png" { } ''
-              ${lib.getExe' pkgs.imagemagick "magick"} \
-                ${./iceman.png} \
-                +level-colors "${base00},${base02}" \
-                -colorspace sRGB \
-                $out
-            '';
+          image = pkgs.runCommandLocal "wallpaper.png" { } ''
+            ${lib.getExe' pkgs.imagemagick "magick"} \
+              ${./iceman.png} \
+              +level-colors "${colors.withHashtag.base00},${halfway}" \
+              -colorspace sRGB \
+              $out
+          '';
         };
       };
 
