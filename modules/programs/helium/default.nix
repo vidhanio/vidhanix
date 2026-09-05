3c29b823a7a8fab29c30507d1bf67d56a1acfa2b
Vidhan Bhatt <me@vidhan.io>
2026-09-05 10:08:28 -0400

    helium: load stylix theme extension

--- modules/programs/helium/default.nix
+++ modules/programs/helium/default.nix
@@ -3,10 +3,100 @@
 
   flake.aspects.helium = {
     homeManager =
-      { inputs', pkgs, ... }:
+      {
+        config,
+        inputs',
+        lib,
+        pkgs,
+        ...
+      }:
       let
         lua = pkgs.formats.lua { };
-        pkg = inputs'.helium.packages.default;
+
+        colors = config.lib.stylix.colors;
+        rgbList =
+          name:
+          map (channel: lib.toInt colors."${name}-rgb-${channel}") [
+            "r"
+            "g"
+            "b"
+          ];
+
+        manifest = pkgs.writers.writeJSON "manifest.json" {
+          manifest_version = 3;
+          name = "Stylix";
+          version = "1.0.0";
+
+          theme = {
+            colors = lib.mapAttrs (_: rgbList) {
+              # `frame` currently does nothing: https://github.com/imputnet/helium/issues/1459
+              frame = "base00";
+              frame_incognito = "base00";
+
+              frame_inactive = "base01";
+              frame_incognito_inactive = "base01";
+
+              toolbar = "base00";
+
+              tab_text = "base05";
+              bookmark_text = "base05";
+              toolbar_text = "base05";
+
+              background_tab = "base01";
+              background_tab_inactive = "base01";
+              background_tab_incognito = "base01";
+              background_tab_incognito_inactive = "base01";
+
+              tab_background_text = "base04";
+              tab_background_text_inactive = "base04";
+              tab_background_text_incognito = "base04";
+              tab_background_text_incognito_inactive = "base04";
+
+              button_background = "base00";
+
+              omnibox_text = "base0D";
+              omnibox_background = "base01";
+
+              ntp_background = "base00";
+              ntp_header = "base01";
+              ntp_link = "base04";
+              ntp_text = "base04";
+            };
+
+            tints =
+              let
+                identity = [
+                  (-1)
+                  (-1)
+                  (-1)
+                ];
+              in
+              {
+                background_tab = identity;
+                buttons = identity;
+                frame = identity;
+                frame_inactive = identity;
+                frame_incognito = identity;
+                frame_incognito_inactive = identity;
+              };
+            images.theme_ntp_background = "wallpaper.png";
+          };
+        };
+        theme = pkgs.runCommandLocal "helium-theme" { } ''
+          mkdir $out
+          ln -s ${manifest} $out/manifest.json
+          ln -s ${config.stylix.image} $out/wallpaper.png
+        '';
+        unwrapped = inputs'.helium.packages.default;
+        pkg = pkgs.symlinkJoin {
+          inherit (unwrapped) pname version meta;
+          paths = [ unwrapped ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/helium \
+              --add-flags ${lib.escapeShellArg "--load-extension=${theme}"}
+          '';
+        };
       in
       {
         home.packages = [ pkg ];
