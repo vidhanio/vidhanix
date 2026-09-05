fdd2efe9ceb40d3e29918be7ce3d73feb58f3437
Vidhan Bhatt <me@vidhan.io>
2026-09-04 21:05:27 -0400

    herdr: align stylix colors with base16 roles

--- modules/programs/herdr/stylix.nix
+++ modules/programs/herdr/stylix.nix
@@ -6,16 +6,16 @@
         programs.herdr.settings.theme.custom = with config.lib.stylix.colors.withHashtag; {
           accent = base0D;
 
-          panel_bg = base01;
+          panel_bg = base00;
           sidebar_bg = "reset";
-          active_row_bg = base00;
+          active_row_bg = base01;
           selection_bg = base02;
 
           surface0 = base02;
           surface1 = base03;
-          surface_dim = base00;
+          surface_dim = base01;
 
-          overlay0 = base04;
+          overlay0 = base03;
           overlay1 = base04;
           text = base05;
           subtext0 = base04;
