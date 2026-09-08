8abf6d6d1c466adc147024569aade188eed5acc9
Vidhan Bhatt <me@vidhan.io>
2026-09-08 11:20:14 -0400

    fish: consolidate configuration

--- modules/programs/fish/default.nix
+++ modules/programs/fish/default.nix
@@ -10,9 +10,39 @@
         users.defaultUserShell = config.programs.fish.package;
       };
     homeManager =
-      { pkgs, ... }:
+      { lib, pkgs, ... }:
       {
         programs.fish.enable = true;
+
+        programs.fish.functions = {
+          fish_greeting = lib.getExe pkgs.pfetch;
+          fish_prompt = ''
+            set -l last_pipestatus $pipestatus
+            set -lx __fish_last_status $status
+            set -l normal (set_color --reset)
+
+            set -l color_cwd $fish_color_cwd
+            set -l suffix '>'
+            if functions -q fish_is_root_user; and fish_is_root_user
+                if set -q fish_color_cwd_root
+                    set color_cwd $fish_color_cwd_root
+                end
+                set suffix '#'
+            end
+
+            set -l bold_flag --bold
+            set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+            if test $__fish_prompt_status_generation = $status_generation
+                set bold_flag
+            end
+            set __fish_prompt_status_generation $status_generation
+            set -l status_color (set_color $fish_color_status)
+            set -l statusb_color (set_color $bold_flag $fish_color_status)
+            set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+            echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status $suffix " "
+          '';
+        };
 
         programs.carapace = {
           enable = true;
@@ -21,6 +51,13 @@
 
         # fish is the login shell; silence the pre-shell login banner.
         home.file.".hushlogin".source = pkgs.emptyFile;
+
+        persist.files = [
+          {
+            file = ".local/share/fish/fish_history";
+            method = "symlink";
+          }
+        ];
       };
   };
 }
