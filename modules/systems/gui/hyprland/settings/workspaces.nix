889276e243d7c6405644ff8cf70509900c91f28a
Vidhan Bhatt <me@vidhan.io>
2026-09-10 13:47:36 -0400

    hyprland(hyprsplit): move to new path format

--- modules/systems/gui/hyprland/settings/workspaces.nix
+++ modules/systems/gui/hyprland/settings/workspaces.nix
@@ -6,13 +6,11 @@
         lua = pkgs.formats.lua { };
       in
       {
-        xdg.configFile."hypr/hyprsplit/init.lua".source = "${pkgs.hyprlandPlugins.hyprsplit.src}/init.lua";
         wayland.windowManager.hyprland = {
-          # TODO: https://github.com/nix-community/home-manager/pull/9918
-          # extraLuaFiles."hyprsplit/init" = {
-          #   autoLoad = false;
-          #   path = "${pkgs.hyprlandPlugins.hyprsplit.src}/init.lua";
-          # };
+          extraLuaFiles."hyprsplit/init" = {
+            autoLoad = false;
+            content = "${pkgs.hyprlandPlugins.hyprsplit.src}/init.lua";
+          };
 
           settings = {
             hs._var = lua.lib.mkRaw ''require("hyprsplit")'';
